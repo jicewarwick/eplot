b045d47b92878d7f9c58ae2779f505cd5dd52c57
Ji Ce <jice@yanfenginv.com>
2019-08-23 18:06:47 +0800

    working(except scatter and scatter 3d)

--- eplot/eplot.py
+++ eplot/eplot.py
@@ -16,22 +16,22 @@ class EchartsBasePlotMethods(PandasObject):
     def __init__(self, data):
         self._parent = data  # can be Series or DataFrame
 
-    def __call__(self, kind='bar', title=None, bins=10, **kwargs):
+    def __call__(self, kind='bar', **kwargs):
         if kind == 'bar':
-            return self.bar(title=title, **kwargs)
+            return self.bar(**kwargs)
         if kind == 'line':
-            return self.line(title=title, **kwargs)
+            return self.line(**kwargs)
         if kind == 'pie':
-            return self.pie(title=title, **kwargs)
+            return self.pie(**kwargs)
         if kind == 'hist':
-            return self.hist(title=title, bins=bins, **kwargs)
+            return self.hist(**kwargs)
         if kind == 'box':
-            return self.box(title=title, **kwargs)
+            return self.box(**kwargs)
         if kind == 'count_plot':
-            return self.count_plot(title=title, **kwargs)
+            return self.count_plot(**kwargs)
 
     @staticmethod
-    def _render_html(chart: RectChart, path: str = None):
+    def _render_html(chart: RectChart, path: str = None) -> None:
         if path is None:
             path = "render.html"
         template_name = "simple_chart.html"
@@ -51,45 +51,6 @@ class EchartsBasePlotMethods(PandasObject):
                 config_items=require_config["config_items"],
                 libraries=require_config["libraries"]))
 
-    @staticmethod
-    def _set_options(figure, config: Mapping[str, Mapping[str, Any]] = None):
-        figure.set_global_opts(title_opts=opts.TitleOpts(**config['title_opts']),
-                               legend_opts=opts.LegendOpts(**config['legend_opts']),
-                               tooltip_opts=opts.TooltipOpts(**config['tooltips_opts'])
-                               )
-        figure.set_series_opts(label_opts=opts.LabelOpts(**config['label_opts']))
-        return figure
-
-    @staticmethod
-    def _combine_options(config: Mapping[str, MutableMapping[str, Any]] = None,
-                         title: str = None, subtitle: str = None,
-                         legend_pos: str = 'center', legend_orient: str = 'vertical',
-                         label_format: str = None) -> Mapping[str, Mapping[str, Any]]:
-        if config is None:
-            config = {'title_opts': {}, 'legend_opts': {}, 'tooltips_opts': {}, 'label_opts': {}}
-        config['title_opts']['title'] = title
-        config['title_opts']['subtitle'] = subtitle
-        config['legend_opts']['legend_pos'] = legend_pos
-        config['legend_opts']['legend_orient'] = legend_orient
-        config['label_opts']['formatter'] = label_format
-
-        return config
-
-    @staticmethod
-    def _split_data(data: Union[pd.DataFrame, pd.Series], columns: Sequence[str] = None) -> Dict:
-        if columns:
-            columns = list(set(columns) & set(data.columns))
-        storage = {}
-        if isinstance(data, pd.Series):
-            storage[data.name] = (data.index.tolist(), data.values.tolist())
-        elif isinstance(data, pd.DataFrame):
-            useful_df = data.loc[:, columns]
-            for name, series in useful_df.iteritems():
-                x = series.index.tolist()
-                y = series.values.tolist()
-                storage[name] = (x, y)
-        return storage
-
     def _render(self, chart, path: str = None) -> Optional[HTML]:
         if path is None:
             return self._render_to_notebook(chart)
@@ -106,14 +67,60 @@ class EchartsBasePlotMethods(PandasObject):
         else:
             raise ValueError('Unknown Format, only [html, png, jpg, jpeg] supported')
 
+    @staticmethod
+    def _set_options(figure, config: Mapping[str, Mapping[str, Any]] = None):
+        figure.set_global_opts(title_opts=opts.TitleOpts(**config['title_opts']),
+                               legend_opts=opts.LegendOpts(**config['legend_opts']),
+                               tooltip_opts=opts.TooltipOpts(**config['tooltips_opts'])
+                               )
+        figure.set_series_opts(label_opts=opts.LabelOpts(**config['label_opts']))
+        return figure
+
+    @staticmethod
+    def _combine_options(config: Mapping[str, MutableMapping[str, Any]] = None,
+                         title: str = None, subtitle: str = None,
+                         legend_orient: str = 'horizontal',
+                         show_label: bool = True, label_format: str = None) -> Dict[str, Dict[str, Any]]:
+        if config is None:
+            config = {'title_opts': {}, 'legend_opts': {}, 'tooltips_opts': {}, 'label_opts': {}}
+        config['title_opts']['title'] = title
+        config['title_opts']['subtitle'] = subtitle
+
+        config['legend_opts']['orient'] = legend_orient
+        # config['legend_opts']['pos_left'] = 'left'
+        # config['legend_opts']['pos_top'] = 'center'
+
+        config['label_opts']['is_show'] = show_label
+        config['label_opts']['formatter'] = label_format
+
+        return config
+
+    @staticmethod
+    def _split_data(data: Union[pd.DataFrame, pd.Series], columns: Sequence[str] = None) -> Dict:
+        storage = {}
+        if isinstance(data, pd.Series):
+            storage[data.name] = (data.index.tolist(), data.values.tolist())
+        elif isinstance(data, pd.DataFrame):
+            columns = list(set(columns) & set(data.columns)) if columns else data.columns
+            useful_df = data.loc[:, columns]
+            for name, series in useful_df.iteritems():
+                x = series.index.tolist()
+                y = series.values.tolist()
+                storage[name] = (x, y)
+        return storage
+
+    # draw funcs
     def bar(self, columns: Sequence[str] = None, path: str = None,
-            config: Mapping[str, MutableMapping[str, Any]] = None,
+            config: Mapping[str, MutableMapping[str, Any]] = None, flip_xy: bool = False,
             **kwargs) -> Optional[HTML]:
 
         bar_fig = pyecharts.charts.Bar()
         for name, (x, y) in self._split_data(self._parent, columns).items():
             bar_fig.add_xaxis(x)
             bar_fig.add_yaxis(name, y)
+
+        if flip_xy:
+            bar_fig.reversal_axis()
 
         config = self._combine_options(config, **kwargs)
         bar_fig = self._set_options(bar_fig, config)
@@ -123,9 +130,11 @@ class EchartsBasePlotMethods(PandasObject):
     def line(self, columns: Sequence[str] = None, path: str = None,
              config: Mapping[str, MutableMapping[str, Any]] = None,
              line_config: Mapping[str, str] = None, **kwargs) -> Optional[HTML]:
+        if line_config is None:
+            line_config = {}
 
         line_fig = pyecharts.charts.Line()
-        for name, x, y in self._split_data(self._parent, columns):
+        for name, (x, y) in self._split_data(self._parent, columns).items():
             line_fig.add_xaxis(x)
             line_fig.add_yaxis(name, y, **line_config)
 
@@ -134,69 +143,80 @@ class EchartsBasePlotMethods(PandasObject):
         return self._render(line_fig, path)
 
     def pie(self, columns: Sequence[str] = None, path: str = None,
+            pie_config: Mapping[str, Any] = None,
             config: Mapping[str, MutableMapping[str, Any]] = None, **kwargs) -> Optional[HTML]:
+        if pie_config is None:
+            pie_config = {}
 
         pie_fig = pyecharts.charts.Pie()
         for name, (x, y) in self._split_data(self._parent, columns).items():
-            pie_fig.add(name, list(zip(x, y)), **kwargs)
+            pie_fig.add(name, list(zip(x, y)), **pie_config)
 
         config = self._combine_options(config, **kwargs)
+        if config['label_opts']['formatter'] is None:
+            config['label_opts']['formatter'] = '{b}:{c}'
         pie_fig = self._set_options(pie_fig, config)
         return self._render(pie_fig, path)
 
-    def hist(self, title: str = None, bins: int = 10, **kwargs):
-        data = self._parent
-        hist_fig = pyecharts.charts.Bar()
-        hist_fig.set_global_opts(title_opts=opts.TitleOpts(title=title))
-        y, x = np.histogram(data, bins=bins)
-        x = x.astype(int).astype(str)
+    def hist(self, bins: int = 10, path: str = None,
+             config: Mapping[str, MutableMapping[str, Any]] = None, **kwargs) -> Optional[HTML]:
+        assert isinstance(self._parent, pd.Series), 'Input must be a pd.Series'
+
+        y, x = np.histogram(self._parent, bins=bins)
+        x = [f'{it:0.2f}' for it in x]
         x_labels = [x[i - 1] + '-' + x[i] for i in range(1, len(x))]
+
+        hist_fig = pyecharts.charts.Bar()
         hist_fig.add_xaxis(x_labels)
-        hist_fig.add_yaxis(data.name, y.tolist(), **kwargs)
-        return self._render_to_notebook(hist_fig)
+        hist_fig.add_yaxis(self._parent.name, y.tolist(), category_gap=0)
 
-    def box(self, title=None, **kwargs):
-        data = self._parent
+        config = self._combine_options(config, **kwargs)
+        hist_fig = self._set_options(hist_fig, config)
+
+        return self._render(hist_fig, path)
+
+    def box(self, name: str = '', columns: Sequence[str] = None, path: str = None,
+            config: Mapping[str, MutableMapping[str, Any]] = None, **kwargs) -> Optional[HTML]:
         box_fig = pyecharts.charts.Boxplot()
-        box_fig.set_global_opts(title_opts=opts.TitleOpts(title=title))
-        if isinstance(data, pd.Series):
-            box_fig.add_xaxis([data.name])
-            box_fig.add_yaxis('', box_fig.prepare_data(data.values.reshape((1, -1)).tolist()))
-        elif isinstance(data, pd.DataFrame):
-            box_fig.add_xaxis(data.columns.tolist())
-            box_fig.add_yaxis('', box_fig.prepare_data(data.values.T.tolist()))
-        return self._render_to_notebook(box_fig)
+        box_fig.add_xaxis([name])
+        for col_name, (_, y) in self._split_data(self._parent, columns).items():
+            box_fig.add_yaxis(col_name, box_fig.prepare_data([y]))
 
-    def count_plot(self, title=None, **kwargs):
-        return self._parent.value_counts().eplot.bar(title=title, **kwargs)
+        config = self._combine_options(config, **kwargs)
+        box_fig = self._set_options(box_fig, config)
+        return self._render(box_fig, path)
 
-    def scatter(self, x, y, category_col=None, title=None, category_name=None, **kwargs):
-        data = self._parent
-        scatterFig = pyecharts.charts.Scatter()
-        scatterFig.set_global_opts(title_opts=opts.TitleOpts(title=title))
+    def count_plot(self, columns: Sequence[str] = None, path: str = None,
+                   config: Mapping[str, MutableMapping[str, Any]] = None,
+                   flip_xy: bool = False, **kwargs) -> Optional[HTML]:
+        return self._parent.value_counts().eplot.bar(columns=columns, path=path, config=config, flip_xy=flip_xy,
+                                                     **kwargs)
+
+    def scatter(self, x: str, y: str, category_col=None, path: str = None,
+                config: Mapping[str, MutableMapping[str, Any]] = None, **kwargs) -> Optional[HTML]:
+        scatter_fig = pyecharts.charts.Scatter()
         if category_col is None:
-            (scatterFig.add_xaxis(data[x].values.tolist())
-             .add_yaxis('', data[y].values.tolist())
-             .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-             )
+            x_val = [int(val) for val in self._parent[x].values.tolist()]
+            y_val = [float(val) for val in self._parent[y].values.tolist()]
+            scatter_fig.add_xaxis(x_val)
+            scatter_fig.add_yaxis(y, y_val)
+        else:
+            for cat, d in self._parent.groupby(category_col):
+                scatter_fig.add_xaxis(d[x].values.tolist())
+                scatter_fig.add_yaxis(cat, d[y].values.tolist())
+        config = self._combine_options(config, show_label=False, **kwargs)
+        scatter_fig = self._set_options(scatter_fig, config)
+        return self._render(scatter_fig, path)
+
+    def scatter3d(self, x, y, z, category_col=None, title=None, category_name=None, **kwargs) -> Optional[HTML]:
+        data = self._parent
+        scatter3d_fig = pyecharts.charts.Scatter3D(title)
+        if category_col is None:
+            scatter3d_fig.add(category_name, data[[x, y, z]].values, **kwargs)
         else:
             for cat, d in data.groupby(category_col):
-                # scatterFig.add(cat, d[x], d[y], **kwargs)
-                (scatterFig.add_xaxis(d[x].values.tolist())
-                 .add_yaxis(cat, d[y].values.tolist())
-                 .set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-                 )
-        return self._render_to_notebook(scatterFig)
-
-    def scatter3d(self, x, y, z, category_col=None, title=None, category_name=None, **kwargs):
-        data = self._parent
-        scatter3dFig = pyecharts.charts.Scatter3D(title)
-        if category_col is None:
-            scatter3dFig.add(category_name, data[[x, y, z]].values, **kwargs)
-        else:
-            for cat, d in data.groupby(category_col):
-                scatter3dFig.add(cat, d[[x, y, z]].values, **kwargs)
-        return scatter3dFig
+                scatter3d_fig.add(cat, d[[x, y, z]].values, **kwargs)
+        return scatter3d_fig
 
 
 pd.Series.eplot = CachedAccessor("eplot", EchartsBasePlotMethods)
--- test.py
+++ test.py
@@ -9,45 +9,49 @@ import eplot.eplot
 
 class EPlotTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.df = pd.DataFrame([np.random.uniform(10, 1, size=1000), np.random.uniform(10, 5, size=1000),
-                                np.random.randint(1, high=10, size=1000), np.random.choice(list('ABCD'), size=1000)],
+        n = 20
+        self.df = pd.DataFrame([np.random.uniform(1, 10, size=n), np.random.uniform(5, 10, size=n),
+                                np.random.randint(1, high=10, size=n), np.random.choice(list('ABCD'), size=n)],
                                index=['col1', 'col2', 'col3', 'col4']).T
 
     def test_df(self):
         print(self.df.head())
 
+    def test_call(self):
+        self.df.eplot(kind='line', path='./html/line_call.html', show_label=False)
+
     def test_line(self):
-        self.df.eplot.line(path='./img/line_plot_test.png')
+        self.df.eplot.line(path='./html/line.html', show_label=False)
 
     def test_bar(self):
         v = self.df.col4.value_counts()
-        v.eplot.bar(title='bar_plot_test', path='./img/bar_plot.png')
+        v.eplot.bar(title='bar_plot_test', path='./html/bar.html')
 
     def test_hist(self):
-        self.df.col1.eplot.hist(bins=8)
+        self.df.col1.eplot.hist(bins=8, path='./html/hist.html')
 
-    def test_scatter(self):
-        self.df.eplot.scatter(x='col1', y='col2')
-        self.df.eplot.scatter(x='col1', y='col2', category_col='col4')
-
-    def test_scatter_3d(self):
-        self.df.eplot.scatter3d(x='col1', y='col2', z='col3')
-        self.df.eplot.scatter3d(x='col1', y='col2', z='col3', category_col='col4')
+    # def test_scatter(self):
+    #     self.df.eplot.scatter(x='col1', y='col2', path='./html/scatter.html')
+    #     self.df.eplot.scatter(x='col1', y='col2', category_col='col4', path='./html/scatter_category.html')
+    #
+    # def test_scatter_3d(self):
+    #     self.df.eplot.scatter3d(x='col1', y='col2', z='col3')
+    #     self.df.eplot.scatter3d(x='col1', y='col2', z='col3', category_col='col4')
 
     def test_pie(self):
         v = self.df.col4.value_counts()
-        v.eplot.pie('pie_plot_test.html')
-        v.eplot.pie('pie_plot_test_radius.html', radius=["40%", "75%"])
-        # v = self.df.col4.value_counts()
-        # v.eplot.pie(inner_radius_from=30, rosetype='rodius')
+        v.eplot.pie(path='./html/pie.html')
+        v.eplot.pie(path='./html/pie_radius.html', pie_config={'radius': ["40%", "75%"]})
+        v = self.df.col4.value_counts()
+        v.eplot.pie(path='./html/pie_rose.html', pie_config={'radius': ["40%", "75%"], 'rosetype': 'rodius'})
         # self.df.head(10).eplot.pie(y='col2', legend_pos='center', legend_orient='')
 
     def test_count_plot(self):
-        self.df.col4.eplot.countplot()
+        self.df.col4.eplot.count_plot(path='./html/count_plot_test.html')
 
     def test_box(self):
-        self.df.eplot.box()
-        self.df.col1.eplot.box()
+        self.df.eplot.box(name='val', columns=['col1', 'col2'], path='./html/box_series_test.html')
+        self.df.col1.eplot.box(path='./html/box_df_test.html')
 
 
 if __name__ == '__main__':
